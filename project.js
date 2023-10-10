97a521127588fc42c1dcb4745d49b2d12778b380
Aidar <ar.ibraev98@gmail.com>
2023-10-10 15:28:03 +0600

    Add additional changes to files

--- project.js
+++ project.js
@@ -1,30 +1,39 @@
+const display = document.querySelector('.display');
+const p = document.createElement('p')
+let displayValue = ""
+
 function makeAddition (a,b) {
-    let sum =  parseInt(a) + parseInt (b)
-    return sum
+    let sum = parseInt(a) + parseInt (b)
+    p.innerText = sum
+    display.appendChild(p)
 }
 
 function makeSubtraction (a, b) {
-    return a - b
+    p.innerText = a - b
+    display.appendChild(p)
 }
 
 function makeMultiply (a, b) {
-    return a * b
+    p.innerText = a * b
+    display.appendChild(p)
 }
 
 function makeDivision(a, b) {
     if (b === 0) {
-      return "Error: Division by zero";
-    }
-    return a / b;
+    p.innerText = "Error: Division by zero";
+    } else {
+    p.innerText = a / b;
+}
+    display.appendChild(p)
 }
 
 let firstNumber = 0;
-let operator = ["+","-","/","*"];
+let currentOperator = "";
 let secondNumber = 0;
 
 function getResult(num1, op, num2) {
     firstNumber = num1;
-    operator = op;
+    currentOperator = op;
     secondNumber = num2
 }
 
@@ -39,6 +48,65 @@ function operate(operator, num1, num2) {
         return makeDivision (num1, num2)
     }
 }
-getResult(15, "/", 5)
-let result = operate(operator, firstNumber, secondNumber)
-console.log(result)
+
+function appendNumber(number) {
+    p.innerText = displayValue += number;
+    p.innerText = display.value = displayValue;
+    display.appendChild(p)
+}
+
+function appendOperator(operator) {
+    p.innerText = displayValue = " " +operator+ " ";
+    p.innerText = display.value = displayValue;
+    display.appendChild(p)
+}
+
+function clearDisplay () {
+    p.innerText = displayValue = "";
+    display.value = displayValue
+    display.appendChild(p)
+}
+
+document.querySelector('#one').addEventListener('click', () => {
+    appendNumber(1)
+})
+document.querySelector('#two').addEventListener('click', () => {
+    appendNumber(2)
+})
+document.querySelector('#three').addEventListener('click', () => {
+    appendNumber(3)
+})
+document.querySelector('#four').addEventListener('click', () => {
+    appendNumber(4)
+})
+document.querySelector('#five').addEventListener('click', () => {
+    appendNumber(5)
+})
+document.querySelector('#six').addEventListener('click', () => {
+    appendNumber(6)
+})
+document.querySelector('#seven').addEventListener('click', () => {
+    appendNumber(7)
+})
+document.querySelector('#eight').addEventListener('click', () => {
+    appendNumber(8)
+})
+document.querySelector('#nine').addEventListener('click', () => {
+    appendNumber(9)
+})
+document.querySelector('#zero').addEventListener('click', () => {
+    appendNumber(0)
+})
+document.querySelector('.add').addEventListener('click', () => {
+    appendOperator('+')
+})
+document.querySelector('.sub').addEventListener('click', () => {
+    appendOperator('-')
+})
+document.querySelector('.division').addEventListener('click', () => {
+    appendOperator('/')
+})
+document.querySelector('.multiply').addEventListener('click', () => {
+    appendOperator('*')
+})
+document.querySelector('.clear').addEventListener('click', clearDisplay)
